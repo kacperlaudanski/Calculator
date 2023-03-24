979096d525b7f8c5d35c3f888238aeb12ba5b95b
Kacper <kacper.laudanski@wp.pl>
2023-03-24 22:33:33 +0100

    calculator appearance

--- index.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.getElementsByClassName("button");
-const arr = [...buttons];
-
-for(let i =0; i<arr.length; i++){
-  arr[i].addEventListener('click', ()=>{
-    arr[i].style.opacity = 1; 
-  })
-};
